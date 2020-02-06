9091cccc56cf07d06fe82154a78a8dd37d0eccdd
Daxxn <46167982+Daxxn@users.noreply.github.com>
2020-02-05 20:16:46 -0800

    Initial Commit And Basic Operation
    Repo set up and Caliburn.micro is up and running.

---
+++ MultiPointInspection/Bootstrapper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using Caliburn.Micro;
+using MultiPointInspection.ViewModels;
+
+namespace MultiPointInspection
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private readonly SimpleContainer _container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes().Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        protected override Object GetInstance(Type service, String key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<Object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(Object instance)
+        {
+            _container.BuildUp(instance);
+        }
+
+        protected override void OnExit(Object sender, EventArgs e)
+        {
+            base.OnExit(sender, e);
+        }
+
+        protected override void OnUnhandledException(Object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            base.OnUnhandledException(sender, e);
+        }
+    }
+}
---
+++ MultiPointInspection/ViewModels/ShellViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiPointInspection.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        private IWindowManager _windowManager;
+        private IEventAggregator _eventAggregator;
+
+        public MainViewModel mainViewModel { get; set; }
+
+        #region Constructors
+        public ShellViewModel(IWindowManager windowManager, IEventAggregator eventAggregator)
+        {
+            _windowManager = windowManager;
+            _eventAggregator = eventAggregator;
+            _eventAggregator.Subscribe(this);
+
+            mainViewModel = new MainViewModel(windowManager, eventAggregator);
+
+            ActivateItem(mainViewModel);
+        }
+        #endregion
+    }
+}
